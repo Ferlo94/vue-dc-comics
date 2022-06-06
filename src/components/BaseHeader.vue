082f157afe971f64a8dde8bea568d814f38dbae5
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-06 14:57:43 +0200

    aggiunto effetto di hover ai link header

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,75 @@
+<template>
+
+  <header class="header">
+
+    <div class="header__logo">
+      <img src="../assets/img/dc-logo.png">
+    </div>
+
+    <nav class="header__nav">
+      <ul>
+        <li class="active"><a href="#">Link</a></li>
+        <li class="active"><a href="#">Link</a></li>
+        <li class="active"><a href="#">Link</a></li>
+        <li class="active"><a href="#">Link</a></li>
+        <li class="active"><a href="#">Link</a></li>
+        <li class="active"><a href="#">Link</a></li>
+        <li class="active"><a href="#">Link</a></li>
+        <li class="active"><a href="#">Link</a></li>
+        <li class="active"><a href="#">Link</a></li>
+        <li class="active"><a href="#">Link</a></li>
+      </ul>
+    </nav>
+
+  </header>
+
+</template>
+
+<script>
+export default {
+    name: 'BaseHeader'
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+@import '../assets/style/mixins.scss';
+
+  .header {
+    
+    @include listInline;
+    align-items: center;
+    background-color: white;
+
+    &__logo {
+      width: 30%;
+    }
+
+    &__nav {
+       width: 60%;
+
+      ul {
+
+        @include listInline;
+
+        a {
+          color: var(--terzo-color);
+          margin: .625rem;
+          text-decoration: none;
+        }
+        .active a:hover {
+          border-bottom: 5px solid var(--primary-color);
+
+        }
+        
+
+      }
+
+      
+
+
+
+    }
+  }
+</style>
