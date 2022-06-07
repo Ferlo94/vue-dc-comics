050af17ff225a39ffce9bc3e39d6e472454018ca
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-07 14:24:07 +0200

    aggiunti spazi per i 12 film nel main

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -2,7 +2,13 @@
   <main class="main">
     <div class="main__container">
         <div class="main__hero"></div>
-    </div>    
+        <div class="main__bottom">
+            <div class="main__container-2">
+                <div class="films" v-for="i in 12" :key="i"></div>
+            </div>
+        </div>
+    </div>
+        
   </main>
 </template>
 
@@ -17,8 +23,9 @@ export default {
 
 .main {
     @include listInline;
-    height: 400px;
+    height: 800px;
     justify-content: center;
+    
 
     
 
@@ -26,8 +33,8 @@ export default {
         @include listInline;
         margin: auto;
         width: 100%;
-        // max-width: var(--container-md);
-        align-items: center;
+        
+        flex-direction: column;
         
         
 
@@ -38,11 +45,38 @@ export default {
 
     }
 
+    &__container-2 {
+        max-width: var(--container-md);
+        @include listInline;
+        flex-wrap: wrap;
+        // background-color: aqua;
+        height: 100%;
+        margin: auto;
+        align-items: center;
+        justify-content: center;
+
+    }
+
     &__hero {
         background-image: url(../assets/img/jumbotron.jpg);
         background-size: cover;
         width: 100%;
-        height: 400px;
+        height: 300px;
+    }
+
+    &__bottom {
+        height: 500px;
+        width: 100%;
+        background-color: black;
+
+    }
+
+    .films {
+
+        width: calc(15%);
+        height: 200px;
+        margin: 5px;
+        background-color: white;
     }
 }
 
